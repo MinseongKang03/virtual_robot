c3accbb63762e7d3f5e24dfedf012dccae6225b4
Team 8872 <teamrobotics8872@gmail.com>
2021-09-14 17:58:55 -0700

    wow

--- TeamCode/src/org/firstinspires/ftc/teamcode/ftc16072/MyFIRSTJavaOpMode.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/ftc16072/MyFIRSTJavaOpMode.java
@@ -3,39 +3,37 @@ package org.firstinspires.ftc.teamcode.ftc16072;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 
 public class MyFIRSTJavaOpMode extends LinearOpMode {
-    private DcMotor motorTest;
-    private DigitalChannel digitalTouch;
-    private DistanceSensor sensorColorRange;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
     private Servo servoTest;
 
 
     @Override
     public void runOpMode() {
-        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        servoTest = hardwareMap.get(Servo.class, "servoTest");
-
-        //touch sensor
-        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left_motor");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right_motor");
+        servoTest = hardwareMap.get(Servo.class, "back_servo");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         waitForStart();
 
-        double tgtPower = 0;
+        double leftY = 0;
+        double leftX = 0;
 
         while (opModeIsActive()) {
-            //for servo motor
-            tgtPower = -this.gamepad1.left_stick_y;
-            motorTest.setPower(tgtPower);
+            //for Dc motor
+            leftY = -this.gamepad1.left_stick_y;
+            leftX = this.gamepad1.left_stick_x;
+            frontLeft.setPower(leftY + leftX);
+            frontRight.setPower(leftY - leftX);
+
             //check to see if we have to move the servo motor
             if (gamepad1.y) {
                 servoTest.setPosition(0);
@@ -45,17 +43,10 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
                 servoTest.setPosition(1);
             }
             telemetry.addData("Servo Position", servoTest.getPosition());
-            telemetry.addData("Target Power", tgtPower);
-            telemetry.addData("Motor Power", motorTest.getPower());
-
-            //checking if button is pressed
-            if (digitalTouch.getState() == false) {
-                //button is pressed
-                telemetry.addData("Button", "PRESSED");
-            } else {
-                //button is not pressed
-                telemetry.addData("Button", "NOT PRESSED");
-            }
+            telemetry.addData("Front Left Motor Power", frontLeft.getPower());
+            telemetry.addData("Front Right Motor Power", frontRight.getPower());
+            telemetry.addData("left_stick_y", gamepad1.left_stick_y);
+            telemetry.addData("left_stick_x", gamepad1.left_stick_x);
 
             telemetry.addData("Status", "Running");
             telemetry.update();
