6f331f865d5f9847436352e86c58857a8ef24c8f
Team 8872 <teamrobotics8872@gmail.com>
2021-09-17 17:42:24 -0700

    wow

--- TeamCode/src/org/firstinspires/ftc/teamcode/ftc16072/MyFIRSTJavaOpMode.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/ftc16072/MyFIRSTJavaOpMode.java
@@ -3,11 +3,9 @@ package org.firstinspires.ftc.teamcode.ftc16072;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
-
 public class MyFIRSTJavaOpMode extends LinearOpMode {
     private DcMotor frontLeft;
     private DcMotor frontRight;
@@ -30,13 +28,17 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
 
         double rightX = 0;
         double leftY = 0;
+        double leftX = 0;
 
         while (opModeIsActive()) {
             //for Dc motor
+            leftX = this.gamepad1.left_stick_x;
             rightX = -this.gamepad1.right_stick_x;
-            leftY = this.gamepad1.left_stick_y;
-            frontLeft.setPower(rightX + leftY);
-            frontRight.setPower(rightX - leftY);
+            leftY = -this.gamepad1.left_stick_y;
+            frontLeft.setPower(rightX - leftY - leftX);
+            backRight.setPower(rightX + leftY + leftX);
+            frontRight.setPower(rightX + leftY - leftX);
+            backLeft.setPower(rightX - leftY + leftX);
 
             //check to see if we have to move the servo motor
             if (gamepad1.y) {
@@ -49,6 +51,8 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
             telemetry.addData("Servo Position", servoTest.getPosition());
             telemetry.addData("Front Left Motor Power", frontLeft.getPower());
             telemetry.addData("Front Right Motor Power", frontRight.getPower());
+            telemetry.addData("Back Left Motor Power", backLeft.getPower());
+            telemetry.addData("Back Right Motor Power", backRight.getPower());
             telemetry.addData("left_stick_y", gamepad1.left_stick_y);
             telemetry.addData("right_stick_x", gamepad1.right_stick_x);
 
